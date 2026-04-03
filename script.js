094cc54944faf3ca5627b7cce5af62f55038948b
AyerisTheKing <ayeristhemadking@gmail.com>
2026-04-03 14:14:43 +0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Version: 3.1
-// Main configuration file and version tracking for Amir Timur TWA
-
-console.log("App Version: 3.1 - Admin panel split into 3 sub-tabs: Management, Analytics, Moderation");
